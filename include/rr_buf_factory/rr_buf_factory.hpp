1c85c14995183885627144db4c68b19dff97ba28
Aaron Spiteri <azzmosphere@gmail.com>
2025-10-18 15:34:57 +1100

    added source code for factory

---
+++ include/rr_buf_factory/rr_buf_factory.hpp
@@ -0,0 +1,26 @@
+#ifndef RR_BUF_FACTORY
+#define RR_BUF_FACTORY
+
+#include "rclcpp/rclcpp.hpp"
+#include "rr_buf_factory/rr_state_maintainer.hpp"
+
+namespace rrobot
+{
+    class RrBufFactory
+    {
+      public:
+
+        /**
+         * @fn initialize
+         * @brief initialize each subscriber, injects logger and state_manager in each
+         * subscriber, and links them to ctl.
+         */
+        virtual void initialize(rclcpp::Node::SharedPtr ctl, RrStateMaintainer state_manager) = 0;
+
+      protected:
+        RrBufFactory() {}
+        ~RrBufFactory() = default;
+    };
+} // namespace rrobot
+
+#endif
